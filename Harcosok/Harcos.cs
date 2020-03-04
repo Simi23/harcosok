cce83a00fa64799d03a31c631b373d70dd9ef3ae
Simon Tamás <44925919+Simi23@users.noreply.github.com>
2020-03-04 15:45:14 +0100

    Add files via upload

---
+++ Harcosok/Harcos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harcosok
+{
+    class Harcos
+    {
+        private string nev;
+        private int eletero;
+        private int harciero;
+
+        public Harcos(string nev, int eletero, int harciero)
+        {
+            this.nev = nev;
+            this.eletero = eletero;
+            this.harciero = harciero;
+        }
+
+        public bool harcol(Harcos harcos)
+        {
+            eletero -= harcos.getHarciero();
+            harcos.setEletero(harcos.getEletero() - harciero);
+
+            if (eletero <= 0 || harcos.getEletero() <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string toString()
+        {
+            return $"{nev}(Harcierő: {harciero}, Életerő: {eletero})";
+        }
+
+        #region Getters & Setters
+        public string getNev()
+        {
+            return nev;
+        }
+
+        public int getEletero()
+        {
+            return eletero;
+        }
+        public int getHarciero()
+        {
+            return harciero;
+        }
+        public void setNev(string nev)
+        {
+            this.nev = nev;
+        }
+        public void setEletero(int eletero)
+        {
+            this.eletero = eletero;
+        }
+        public void setHarciero(int harciero)
+        {
+            this.harciero = harciero;
+        }
+        #endregion
+    }
+}
---
+++ Harcosok/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Harcosok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Harcos h1 = new Harcos("Erik", 100, 12);
+            Harcos h2 = new Harcos("Powerpoint", 90, 13);
+
+            while (true)
+            {
+                bool eredmeny = h1.harcol(h2);
+                Console.WriteLine(h1.toString());
+                Console.WriteLine(h2.toString());
+                Console.WriteLine("");
+
+                if (eredmeny)
+                {
+                    Console.WriteLine("A harcnak vége.");
+                    break;
+                }
+            }
+        }
+    }
+}
